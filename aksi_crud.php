89cdab6bbe932f6e082a92b4ded726383febde6d
yusuparipin30 <yusuparipin30@gmail.com>
2022-10-03 01:12:12 +0700

    tambah crud

--- aksi_crud.php
+++ aksi_crud.php
@@ -36,5 +36,61 @@ $simpan =mysqli_query($koneksi, "INSERT INTO twarga(nama,jenis_kelamin,alamat, k
 }
 
 
+//proses ubah  data 1
+//kita uji jika tombol ubah di klik 1.2
+if (isset($_POST['bubah'])){
+    //persiapan proses ubah data , bkin variabel $simpan
+$ubah =mysqli_query($koneksi, "UPDATE twarga SET
+                                                nama = '$_POST[tnama]',
+                                                jenis_kelamin = '$_POST[tkelamin]',
+                                                alamat = '$_POST[talamat]',
+                                                kamar = '$_POST[tkamar]',
+                                                status_kawin = '$_POST[tkawin]',
+                                                status_tinggal = '$_POST[ttinggal]',
+                                                status_hunian = '$_POST[thunian]',
+                                                pekerjaan = '$_POST[tpekerjaan]',
+                                                dokumen = '$_POST[tdokumen]',
+                                                tgl_masuk = '$_POST[ttanggal_masuk]'
+                                                WHERE id_warga = '$_POST[id_warga]'
+                                                ");
+
+            //melakukan pengujian apabila data di ubah
+            if ($ubah){
+                echo "<script>
+                    alert('ubah data sukses!');
+                    document.location='index.php';
+                </script>";
+            }else{
+                echo "<script>
+                alert('ubah data gagal!');
+                document.location='index.php';
+            </script>";
+            }
+}
+
+
+//proses hapus  data 1
+//kita uji jika tombol hapus di klik 1.2
+if (isset($_POST['bhapus'])){
+    //persiapan proses hapus data , bkin variabel $simpan
+$hapus =mysqli_query($koneksi, "DELETE FROM twarga WHERE id_warga = '$_POST[id_warga]'");
+
+            //melakukan pengujian apabila data di hapus
+            if ($hapus){
+                echo "<script>
+                    alert('hapus data sukses!');
+                    document.location='index.php';
+                </script>";
+            }else{
+                echo "<script>
+                alert('hapus data gagal!');
+                document.location='index.php';
+            </script>";
+            }
+}
+
+
+
+
 
 ?>
--- index.php
+++ index.php
@@ -3,34 +3,11 @@
 //4.memanggil koneksi di indek html. 5.menampilkan data dri data base 
 //6.membuat simpan data/tambah data di file aksi_crud.php
 
-
-
 //panggil koneksi database
 include "koneksi.php";
 
 
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!doctype html>
 <html lang="en">
@@ -79,10 +56,10 @@ include "koneksi.php";
                 <th>No.</th>
                 <th>Nama</th>
                 <th>Jns Kelamin</th>  
-                <th>Alamat</th> 
+                <th>Alamat Palm</th> 
                 <th>Kamar</th>
                 <th>Stus Kawin</th>
-                <th>Stus Tinggal</th>
+                <th>S_Tinggal</th>
                 <th>Stus Hunian</th>
                 <th>Pekerjaan</th>
                 <th>Dokumen</th>
@@ -115,20 +92,170 @@ include "koneksi.php";
                 <td><?= $data['dokumen'] ?></td>
                 <td><?= $data['tgl_masuk'] ?></td>
                 <td>
-                  <a href="#" class="btn btn-warning">Edit</a>
-                  <a href="#" class="btn btn-danger">Hapus</a>
+                  <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalUbah<?= $no ?>">Ubah</a>
+                  <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalHapus<?= $no ?>">Hapus</a>
                 </td>
               </tr>
+                  <!-- menampilkan modal ubah, looping dalam perulangan while -->
+                 <!--awal Modal ubah /$no biar id unik -->
+                 <div class="modal fade modal-lg" id="modalUbah<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="modalUbahLabel">Form ubah data warga</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <!-- AWAL FORM -->
+                        <form action="aksi_crud.php" method="POST">
+                          <input type="hidden" name="id_warga" value="<?=$data['id_warga']?>">
+                        <div class="modal-body">  
+
+                                <div class="mb-3">
+                                  <label class="form-label">Nama</label>
+                                  <input type="text" class="form-control" name="tnama" 
+                                  value="<?= $data['nama']?>" placeholder="Masukan Nama Lengkap">
+                                </div>
+
+                                <div class="mb-3">
+                              <label class="form-label">Jenis Kelamin</label>
+                              <select class="form-select" name="tkelamin">
+                                <option value="<?= $data['jenis_kelamin']?>"><?= $data['jenis_kelamin']?></option>
+                                <option value="laki - laki">Laki - Laki</option>
+                                <option value="perempuan">Perempuan</option>                        
+                              </select>
+                            </div>
+                            
+
+                            <div class="mb-3">
+                              <label class="form-label">Alamat</label>
+                              <textarea class="form-control" name="talamat" rows="3"><?= $data['alamat']?></textarea>
+                            </div>
+
+                            <div class="mb-3">
+                              <label class="form-label">Kamar</label>
+                              <select class="form-select" name="tkamar">
+                                <option value="<?= $data['kamar']?>"></option>
+                                <option value="kamar bawah">Kamar Bawah</option>
+                                <option value="atas sisi toilet">Atas Sisi Toilet</option>
+                                <option value="atas sisi tangga">Atas Sisi Tangga</option>
+                                <option value="satu unit rumah">Satu Unit Rumah</option>
+                              </select>
+                            </div>
+
+                            <div class="mb-3">
+                              <label class="form-label">Status Perkawinan</label>
+                              <select class="form-select" name="tkawin">
+                                <option value="<?= $data['status_kawin']?>"><?= $data['status_kawin']?></option>
+                                <option value="menikah">Menikah</option>
+                                <option value="belum menikah">Belum Menikah</option>
+                                <option value="janda">Janda</option>
+                                <option value="duda">Duda</option>
+                              </select>
+                            </div>              
+
+                                <div class="mb-3">
+                              <label class="form-label">Status Tinggal</label>
+                              <select class="form-select" name="ttinggal">
+                                <option value="<?= $data['status_tinggal']?>"><?= $data['status_tinggal']?></option>
+                                <option value="sendiri">Sendiri</option>
+                                <option value="bersama keluarga">Bersama Keluarga</option>
+                                <option value="bersama sudara">Bersama Saudara</option>
+                                <option value="bersama teman">Bersama Teman</option>
+                              </select>
+                            </div>
+
+
+                            <div class="mb-3">
+                              <label class="form-label">Status Hunian</label>
+                              <select class="form-select" name="thunian">
+                                <option value="<?= $data['status_hunian']?>"><?= $data['status_hunian']?></option>
+                                <option value="pemilik">Pemilik</option>
+                                <option value="penyewa">Penyewa</option>                               
+                              </select>
+                            </div>
+
+                                <div class="mb-3">
+                                  <label class="form-label">Pekerjaan</label>
+                                  <input type="text" class="form-control" name="tpekerjaan" value="<?= $data['pekerjaan']?>" 
+                                  placeholder="Masukan nama pekerjaan">
+                                </div>
+
+                                <div class="mb-3">
+                                  <label class="form-label">Dokumen</label>
+                                  <select class="form-select" name="tdokumen">
+                                    <option value="<?= $data['dokumen']?>"><?= $data['dokumen']?></option>
+                                    <option value="ktp">KTP</option>
+                                    <option value="ktp kk buku nikah">KTP/KK/B_NUKAH</option>
+                                    <option value="kk buku nikah">KK/B_NIKAH</option>
+                                  </select>
+                                </div>
+
+
+                                <div class="mb-3">
+                                        <label class="form-label">Tanggal masuk</label>
+                                        <input type="date" name="ttanggal_masuk" class="form-control" value="<?= $data['tgl_masuk']?>"  placeholder="masukan tanggal">
+                                    </div>
+                                
+                        
+
+
+                         
+                        </div>
+                        <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" name="bubah">Ubah</button>
+                          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Keluar</button>
+                         
+                        </div>
+                        
+                      </form>
+                       <!-- ahir form -->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- ahir modal ubah -->
+
+
+                     <!--awal Modal hapus /$no biar id unik -->
+                 <div class="modal fade modal-lg" id="modalHapus<?= $no ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="modalHapusLabel">konfirmasi hapus data</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <!-- AWAL FORM -->
+                      <form action="aksi_crud.php" method="POST">
+                          <input type="hidden" name="id_warga" value="<?=$data['id_warga']?>">
+                        <div class="modal-body">  
+
+                            <h5 class="text-center">Apakah anda yakin akan menghapus data ini?<br>
+                            <span class="text-danger"><?=$data['nama']?> - <?=$data['alamat']?></span>
+                            </h5>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" name="bhapus">Ok, Hapus !</button>
+                          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Tidak</button>
+                         
+                        </div>
+                        
+                      </form>
+                       <!-- ahir form -->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- ahir modal hapus -->
+
+
+
+
               <!-- tutup pengulangan  -->
               <?php endwhile; ?>
             </table>
           <!-- ahir tabel -->
 
-                  <!--awal Modal -->
-  
-                
-
-
+                  <!--awal Modal tambah-->
                   <div class="modal fade modal-lg" id="modalTambah" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
                       <div class="modal-content">
@@ -155,17 +282,6 @@ include "koneksi.php";
                               </select>
                             </div>
                             
-                            <!-- <p  class="form-label">Jenis Kelamin</p>
-                            <div class="mb-3 form-check">
-                            <label class="form-check-label">Perempuan</label>
-                              <input class="form-check-input" type="radio" name="tkelamin">
-                              </div>
-                              <div class="mb-3 form-check">
-                                  <label class="form-check-label">Laki-laki</label>
-                                  <input class="form-check-input" type="radio" name="tkelamin">
-                                </div>
-                              -->
-                               
 
                             <div class="mb-3">
                               <label class="form-label">Alamat</label>
@@ -192,17 +308,7 @@ include "koneksi.php";
                                 <option value="janda">Janda</option>
                                 <option value="duda">Duda</option>
                               </select>
-                            </div>
-                            <!-- <p  class="form-label">Status Perkawinan</p>
-                            <div class="mb-3 form-check">
-                            <label class="form-check-label">menikah</label>
-                              <input class="form-check-input" type="radio" name="tperkawinan">
-                              </div>
-                              <div class="mb-3 form-check">
-                                  <label class="form-check-label">Belum Menikah</label>
-                                  <input class="form-check-input" type="radio" name="tperkawinan">
-                                </div>     -->
-
+                            </div>              
 
                                 <div class="mb-3">
                               <label class="form-label">Status Tinggal</label>
@@ -224,15 +330,6 @@ include "koneksi.php";
                                 <option value="penyewa">Penyewa</option>                               
                               </select>
                             </div>
-                            <!-- <p  class="form-label">Status Hunian</p>
-                            <div class="mb-3 form-check">
-                            <label class="form-check-label">Pemilik</label>
-                              <input class="form-check-input" type="radio" name="thunian">
-                              </div>
-                              <div class="mb-3 form-check">
-                                  <label class="form-check-label">Penyewa</label>
-                                  <input class="form-check-input" type="radio" name="thunian">
-                                </div>     -->
 
                                 <div class="mb-3">
                                   <label class="form-label">Pekerjaan</label>
@@ -271,7 +368,7 @@ include "koneksi.php";
                       </div>
                     </div>
                   </div>
-                  <!-- ahir modal -->
+                  <!-- ahir modal tambah -->
 
 
 
